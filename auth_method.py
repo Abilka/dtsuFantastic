ea58b1b71ff278aa8ff4ae63c132962e016e705a
egor <igeg91999@gmail.com>
2022-03-18 21:12:44 +0000

    auth

---
+++ auth_method.py
@@ -0,0 +1,8 @@
+from main import *
+
+auth_method = [
+    [AuthMethod('VK', [Input('Логин'), Input("Пароль", True)])],
+    [AuthMethod('GitHub', [Input('Логин'), Input('Пароль', True)])],
+    [AuthMethod('Google', [Input('Логин'), Input('Пароль', True)])],
+    [AuthMethod('Telegram', [Input('Логин'), Input('Пароль', True)])],
+]
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import typing
 
+
 class Input:
-    def __init__(self, name: str, is_password: bool=False):
+    def __init__(self, name: str, is_password: bool = False):
         self.name = name
         self.is_hide = is_password
+
 
 class AuthMethod:
     def __init__(self, name: str, input_list: typing.List[Input]):
         ...
 
 
-
-
 if __name__ == '__main__':
-    auth_method = [    AuthMethod('VK', [Input('Логин'), Input("Пароль", True)])]
+    pass
