e0757aec55c947dd8deaaeb23c828d74eb224199
egor <igeg91999@gmail.com>
2022-03-19 17:37:41 +0000

    add error_window.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import auth
 import auth_method
 import error_window
-
-
-def check_service(service: auth_method.AuthMethod):
-    root = auth.MainApp()
-    result = root.generate_auth(service.inputs, service.name)
-    del root
-    print(result)
-    return service.func_check(*result)
+from tkinter import *
 
 if __name__ == '__main__':
     for service in auth_method.auth_method:
-        while check_service(service) is not True:
-            error_window.MainApp()
+        root = auth.MainApp()
+        result = root.generate_auth(service.inputs, service.name)
+
+
+        print(result)
+        while service.func_check(*result) is not True:
+            error_window.MainApp().mainloop()
+
+            root = auth.MainApp()
+            result = root.generate_auth(service.inputs, service.name)
+            print(result)
+
 
 
