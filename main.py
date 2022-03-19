23aa8d49967636ae7436abe24e3b73f2f59589b6
Abilka <alex.161@mail.ru>
2022-03-19 13:03:11 +0300

    добавлен майн ру

--- main.py
+++ main.py
@@ -2,11 +2,56 @@ import typing
 
 import auth
 import auth_method
+import tkinter
+import typing
+from tkinter import *
+
+class MainApp(Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.config_app()
+
+    def generate_auth(self, input: typing.List[auth_method.Input], service: str):
+        self.entry_list: typing.List[tkinter.StringVar] = []
+        count = len(input)
+
+        # Сервис
+        self.service = Label(self, text=service, bg='#fff', font=14, fg='#347aeb')
+        self.service.grid(row=1, column=0, pady=(10, 10))
+
+        for i in range(count):
+            self.entry_list.append(StringVar())
+            Label(self, text=input[i].name, bg='#fff', fg='#000').grid(row=count + 1 + i, column=0, pady=3)
+            Entry(self, width=20, textvariable=self.entry_list[i], show='*' if input[i].is_hide else None, bg='#fff').grid(
+                row=count + 2 + i, column=0, padx=100)
+            count += 1
+
+        next_btn = Button(self, width=10, text='Далее', command=self.btn_next, bg='#fff', fg='#000')
+        next_btn.grid(
+            row=1000, column=0, pady=20
+        )
+
+        self.img = PhotoImage(file='img/DDoS-Guard_logo.svg.png')
+        bg_logo = Label(self, image=self.img, bg='#fff')
+        bg_logo.grid(row=0, column=0, pady=10)
+
+        self.mainloop()
+        return self.btn_next()
+
+    def btn_next(self):
+        self.quit()
+        return list(map(lambda x: x.get(), self.entry_list))
+
+    def config_app(self):
+        self.title('DDoS case')
+        self.config(bg='#fff')
+        self.geometry('350x350+450+200')
+        self.iconphoto(False, PhotoImage(file='img/DDoS-Guard_logo.svg.png'))
+        self.resizable(False, False)
+
 
 if __name__ == '__main__':
-    print(auth_method.auth_method)
     for service in auth_method.auth_method:
-        root = auth.MainApp()
-        result = root.generate_auth(service.inputs, service.name)
-        del root
+        result = MainApp().generate_auth(service.inputs, service.name)
         print(result)
