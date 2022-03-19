05502c0904e93bb27188361123a6753fafe197de
Abilka <alex.161@mail.ru>
2022-03-19 13:24:46 +0300

    багфикс интерфейса

--- auth.py
+++ auth.py
@@ -27,8 +27,8 @@ class MainApp(Tk):
         for i in range(count):
             self.entry_list.append(StringVar())
             Label(self, text=input[i].name, bg='#fff', fg='#000').grid(row=count + 1 + i, column=0, pady=3)
-            Entry(self, width=20, textvariable=self.entry_list[i], show='*' if input[i].is_hide else None, bg='#fff').grid(
-                row=count + 2 + i, column=0, padx=100)
+            Entry(self, width=20, textvariable=self.entry_list[i], show='*' if input[i].is_hide else None,
+                  bg='#fff', fg='#000').grid(row=count + 2 + i, column=0, padx=100)
             count += 1
 
         next_btn = Button(self, width=10, text='Далее', command=self.btn_next, bg='#fff', fg='#000')
@@ -36,7 +36,7 @@ class MainApp(Tk):
             row=1000, column=0, pady=20
         )
 
-        self.img = PhotoImage(file='img/DDoS-Guard_logo.svg.png')
+        self.img = PhotoImage(file='img/DDoS-Guard_logo.svg.png', master=self)
         bg_logo = Label(self, image=self.img, bg='#fff')
         bg_logo.grid(row=0, column=0, pady=10)
 
@@ -51,7 +51,7 @@ class MainApp(Tk):
         self.title('DDoS case')
         self.config(bg='#fff')
         self.geometry('350x350+450+200')
-        self.iconphoto(False, PhotoImage(file='img/DDoS-Guard_logo.svg.png'))
+        self.iconphoto(False, PhotoImage(file='img/DDoS-Guard_logo.svg.png', master=self))
         self.resizable(False, False)
 
 
