e51f9ef9fcd89f5dfba5540b6c88807e055f3b8c
vanya-61 <ivan.romanchenko@internet.ru>
2022-03-20 10:22:17 +0300

    FIX

--- startwin.py
+++ startwin.py
@@ -16,13 +16,13 @@ class App(Tk):
         idraw = ImageDraw.Draw(img)
         text = 'Добро пожаловать в систему!\n\nПройдите авторизацию для продолжения использования системы'
         font = ImageFont.truetype('arial.ttf', size=16)
-        idraw.text((160, 160), text, font=font)
+        idraw.text((160, 160), text, font=font, fg='#000')
         render = ImageTk.PhotoImage(img)
         initil = Label(self, image=render)
         initil.image = render
         initil.grid()
 
-        Button(self, text='Далее', width=10, height=1, font=("Arial Bold", 10), bg='white').grid(
+        Button(self, text='Далее', width=10, height=1, font=("Arial Bold", 10), bg='white', fg='#000').grid(
             row=0, column=0, pady=(160, 0), padx=(0, 650)
         )
 
