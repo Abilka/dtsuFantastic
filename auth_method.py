8680e00483e4e2cb93c2656c028950ff5a8db059
Abilka <alex.161@mail.ru>
2022-03-20 09:58:56 +0300

    new methods auth

--- auth_method.py
+++ auth_method.py
@@ -10,24 +10,26 @@ class Input:
 
 
 class AuthMethod:
-    def __init__(self, name: str, input_list: typing.List[Input], func_check):
+    def __init__(self, name: str, input_list: typing.List[Input], func_check, skip=False):
         self.name = name
         self.inputs = input_list
         self.func_check = func_check
+        self.is_skip = skip
 
     def is_valid(self) -> bool:
         return self.func_check()
 
 
 auth_method = [
-    AuthMethod('GosUslugi', [Input('Логин'), Input("Пароль", True)], methods.Check().gosuslugi),
     AuthMethod('Стандартная авторизация', [Input('Логин'), Input("Пароль", True)], methods.Check().standart),
     AuthMethod('Pincode', [Input('Код')], methods.Check().pin),
     AuthMethod('Google Auth', [Input('Код из приложения')], methods.Check().totp),
     AuthMethod('ВКонтакте', [Input('Логин'), Input("Пароль", True)], methods.Check().vk),
     AuthMethod('Apple ID', [Input('Логин'), Input("Пароль", True)], methods.Check().apple_id),
     AuthMethod('GitHub', [Input('Логин'), Input("Пароль", True)], methods.Check().github),
-    AuthMethod('Pochta (Почта Росcии)', [Input('Логин'), Input("Пароль", True)], methods.Check().pochta),
+    AuthMethod('GosUslugi', [Input('Логин'), Input("Пароль", True)], methods.Check().gosuslugi),
+    AuthMethod('Sber', [Input('Логин'), Input("Пароль", True)], methods.Check().sber, True),
+    # AuthMethod('Steam', [Input('Логин'), Input("Пароль", True)], methods.Check().steam, True),
 
     # AuthMethod('Google', [Input('Логин'), Input('Пароль', True)]),
     # AuthMethod('Telegram', [Input('Номер')]),
--- main.py
+++ main.py
@@ -7,13 +7,12 @@ from tkinter import *
 if __name__ == '__main__':
     for service in auth_method.auth_method:
         root = auth.MainApp()
-        result = root.generate_auth(service.inputs, service.name)
+        result = root.generate_auth(service.inputs, service.name, service.is_skip)
 
 
-        print(result)
-        while service.func_check(*result) is not True:
+        while service.func_check(*result) is not True and not (result[0] == '' and service.is_skip):
             error_window.MainApp()
 
             root = auth.MainApp()
-            result = root.generate_auth(service.inputs, service.name)
+            result = root.generate_auth(service.inputs, service.name, service.is_skip)
             print(result)
